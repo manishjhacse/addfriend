ca7d65dc09c9bfe03b7bec610fcd73a4f1867bb4
Manish Kumar Jha <123859854+manishjhacse@users.noreply.github.com>
2023-09-02 00:54:51 +0530

    added some new feature

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 const friend=document.querySelector("#friend")
+const frndStatus=document.querySelector("#connect-status")
+let isFriend=false;
+friend.addEventListener("click",()=>{
+    if(isFriend){
+        friend.textContent="Add Friend";
+        friend.classList.remove("added")
+        frndStatus.textContent="Strangers"
+        frndStatus.style.color="rgba(204, 19, 19, 0.697)"
+    }
+    else{
+        friend.textContent="Remove"
+        friend.classList.add("added")
+        frndStatus.textContent="Friends"
+        frndStatus.style.color="rgba(19, 19, 139, 0.819)"
+    }
+    isFriend=!isFriend;
+})
+const info=document.querySelector("#info")
+const about=document.querySelector("#about-section")
+info.addEventListener("click",()=>{
+    if(about.classList.contains("hidden"))
+    {
+        about.classList.remove("hidden")
+        about.classList.add("show")
+        info.textContent="Hide"
+    }else{
+        about.classList.add("hidden")
+        about.classList.remove("show")
+        info.textContent="About"
+    }
+})
